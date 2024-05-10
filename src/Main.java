af62a3ee14f218598920e84a5e83c8604e93076c
Keamogetswe Mafora <keamogetswemafora@gmail.com>
2024-05-10 14:27:08 +0200

    First change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
+import java.util.Scanner;
+
+public class Main{
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("");
+        String name = input.next();
+        System.out.print("")   ;
         System.out.println("Hello world!");
     }
 }
